f4f85bf4941e114234f53c6b8d36ce57463f49d2
halfblood <arashyadegari0402@gmail.com>
2023-09-02 11:55:19 +0330

    remove useless import

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
-from tkinter import Text
 from models import *
 from bidi.algorithm import get_display
 import edu
